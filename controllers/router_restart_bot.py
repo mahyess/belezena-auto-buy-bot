bf7a09e15b436f9cd48282f6f4e09c7cbc5406b1
Mahesh <mahyess@users.noreply.github.com>
2021-08-16 19:18:50 +0545

    selenium find element logic change

--- controllers/router_restart_bot.py
+++ controllers/router_restart_bot.py
@@ -29,6 +29,11 @@ def router_restart():
             )
         ).click()
         WebDriverWait(driver, 5).until(
+            EC.frame_to_be_available_and_switch_to_it(
+                (By.CLASS_NAME, "fancybox-iframe")
+            )
+        )
+        WebDriverWait(driver, 5).until(
             EC.element_to_be_clickable(
                 (
                     By.XPATH,
