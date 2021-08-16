6f53daecda1ba389c87b161a7d0c4b3a92a5e4f8
Mahesh <mahyess@users.noreply.github.com>
2021-08-16 21:02:59 +0545

    ping for windows

--- helpers/ping_checker.py
+++ helpers/ping_checker.py
@@ -1,10 +1,12 @@
+import platform
 import subprocess
 import time
 
 
 def ping_until_up(site="www.google.com"):
+    param = "-n" if platform.system().lower() == "windows" else "-c"
     while True:
-        status = subprocess.run(["ping", "-c", "3", site], capture_output=True)
+        status = subprocess.run(["ping", param, "3", site], capture_output=True)
         if status.returncode == 0:
             return
         print(f"{site} is down...")
