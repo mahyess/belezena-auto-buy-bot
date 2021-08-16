c46bb462426203546889946354b5c821713fffcb
Mahesh <mahyess@users.noreply.github.com>
2021-08-16 19:11:31 +0545

    selenium find element logic change

--- controllers/router_restart_bot.py
+++ controllers/router_restart_bot.py
@@ -1,7 +1,10 @@
 import time
 from selenium import webdriver
 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def router_restart():
@@ -14,8 +17,26 @@ def router_restart():
             driver.find_element_by_id("Loginuser").send_keys("admin")
             driver.find_element_by_id("LoginPassword").send_keys("5e65a942")
             driver.find_element_by_id("acceptLogin").click()
-        driver.find_element_by_xpath("//*[.//*[text()='REBOOT']]").click()
-        driver.find_element_by_xpath("//*[.//*[text()='Yes, Reboot']]").click()
+        WebDriverWait(driver, 5).until(
+            EC.frame_to_be_available_and_switch_to_it((By.ID, "basefrm"))
+        )
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//a[.//span[text()='REBOOT']]",
+                )
+            )
+        ).click()
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//a[.//span[text()='Yes, Reboot']]",
+                )
+            )
+        ).click()
+
         time.sleep(90)
 
     except Exception as e:
