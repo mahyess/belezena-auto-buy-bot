251401b3aaa3f6d766a2830ac3637ac57a719b4b
Mahesh <mahyess@users.noreply.github.com>
2021-08-18 19:04:03 +0545

    stdout and stderr in ping check

--- helpers/ping_checker.py
+++ helpers/ping_checker.py
@@ -2,6 +2,13 @@ import platform
 import subprocess
 import time
 
+import os
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    DEVNULL = os.open(os.devnull, os.O_RDWR)
+
 
 def ping_until_up(root, site="www.belezanaweb.com.br"):
     try:
@@ -10,14 +17,14 @@ def ping_until_up(root, site="www.belezanaweb.com.br"):
             status_1 = subprocess.run(
                 ["ping", param, "3", "www.belezanaweb.com.br"],
                 capture_output=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stdin=DEVNULL,
+                stderr=DEVNULL,
             )
             status_2 = subprocess.run(
                 ["ping", param, "3", "www.useragentstring.com"],
                 capture_output=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stdin=DEVNULL,
+                stderr=DEVNULL,
             )
             if status_1.returncode == 0 and status_2.returncode == 0:
                 return
