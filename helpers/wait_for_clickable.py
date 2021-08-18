3d7dae221add4d00a36207a174f0e08012eca5d3
Mahesh <mahyess@users.noreply.github.com>
2021-08-19 01:07:31 +0545

    remove waitable print line

--- helpers/wait_for_clickable.py
+++ helpers/wait_for_clickable.py
@@ -5,6 +5,5 @@ def wait_for_clickable_and_click(element):
     try:
         element.click()
     except Exception as e:
-        print(e)
         time.sleep(2)
         wait_for_clickable_and_click(element)
