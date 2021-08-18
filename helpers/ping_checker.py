d7fd1def68eabc8133277abd3d535dc742c3e265
Mahesh <mahyess@users.noreply.github.com>
2021-08-18 19:23:10 +0545

    stdout and stderr in ping check

--- helpers/ping_checker.py
+++ helpers/ping_checker.py
@@ -14,20 +14,14 @@ def ping_until_up(root, site="www.belezanaweb.com.br"):
     try:
         param = "-n" if platform.system().lower() == "windows" else "-c"
         while True:
-            status_1 = subprocess.run(
-                ["ping", param, "3", "www.belezanaweb.com.br"],
-                # capture_output=True,
-                stdin=DEVNULL,
-                stderr=DEVNULL,
+            status_1 = (
+                subprocess.call(["ping", param, "3", "www.belezanaweb.com.br"]) == 0
             )
-            status_2 = subprocess.run(
-                ["ping", param, "3", "www.useragentstring.com"],
-                # capture_output=True,
-                stdin=DEVNULL,
-                stderr=DEVNULL,
+            status_2 = (
+                subprocess.call(["ping", param, "3", "www.useragentstring.com"]) == 0
             )
             root.show_message_box("from ping until up", status_1)
-            if status_1.returncode == 0 and status_2.returncode == 0:
+            if status_1 and status_2:
                 return
             print("www.belezanaweb.com.br or www.useragentstring.com is down...")
             time.sleep(5)
