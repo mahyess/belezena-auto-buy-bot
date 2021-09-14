d6df91f5102ff540f7c6496ba825a8d3946041cc
Mahesh <mahyess@users.noreply.github.com>
2021-09-15 04:43:04 +0545

    try clicking close banner if available in wait clickable func

--- helpers/wait_for_clickable.py
+++ helpers/wait_for_clickable.py
@@ -3,6 +3,13 @@ import time
 
 def wait_for_clickable_and_click(element):
     try:
+        try:
+            element.find_element_by_xpath(
+                "//*[contains(@class, 'banner-close-button')"
+            ).click()
+        except:
+            pass
+
         element.click()
     except Exception as e:
         time.sleep(2)
