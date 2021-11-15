d807091102dfd3bc6f3471c5e9a9417e27d1ae2a
Mahesh <mahyess@users.noreply.github.com>
2021-11-15 21:09:25 +0545

    update stock working

--- controllers/bots/update_beleza_stock.py
+++ controllers/bots/update_beleza_stock.py
@@ -1,10 +1,11 @@
 import csv
 import time
 import requests
+from lxml import html
+from selenium.webdriver.common.action_chains import ActionChains
 
 from selenium.webdriver.support.wait import WebDriverWait
 from controllers.bots.helpers.mercado_accounts import change_accounts, get_accounts
-from bs4 import BeautifulSoup
 
 from helpers.wait_for_clickable import wait_for_clickable_and_click
 from helpers.user_agent import random_user_agent
@@ -43,35 +44,26 @@ def beleza_stock_scraper(name):
                 return s.post(url, data=data).content
 
     url = f"https://www.belezanaweb.com.br/busca?q={name}"
-    page = BeautifulSoup(fetch(url), "lxml")
-    products = page.find_all("div", {"class": "showcase-item"})
+
+    page = fetch(url)
+    tree = html.fromstring(page)
+    products = tree.xpath(
+        f"//div[contains(@class, 'showcase-item') and .//img[contains(translate(@alt, '{name.upper()}', '{name.lower()}'), '{name.lower()}')]]"
+    )
+
     for product in products:
-        if name.lower() in product.text.lower():
-            if "Avise-me" in product.text:
-                print("Avise-me")
-                return 0
-            if "Compre Agora" in product.text:
-                print("Compre")
-                return 5
+        if "showcase-item-unavailable" in product.classes:
+            print("unavailable")
+            return 0
+
+        print("available")
+        return 5
+
     print("Nothing")
     return 0
 
 
 def bot(root, details=None, driver=None):
-    gender_dict = {
-        "M": "gender-input-1",
-        "F": "gender-input-2",
-    }
-    # address_label = "casa"
-    address_type_dict = {
-        "casa": "HOME",
-        "apartamento": "APARTMENT",
-        "caixa postal": "POST_OFFICE_BOX",
-        "condomínio": "CONDOMINIUM",
-        "comercial": "BUSINESS",
-        "rural": "RURAL",
-        "outro": "OTHER",
-    }
     using_param_driver = False
     if not driver:
         options = Options()
@@ -163,8 +155,29 @@ def bot(root, details=None, driver=None):
                 waiter.until(lambda d: "true" in spinner.get_attribute("aria-hidden"))
                 update()
 
+        state_active_to_paused_changer = ActionChains(driver)
+        state_active_to_paused_changer.send_keys(Keys.ARROW_DOWN)
+        state_active_to_paused_changer.send_keys(Keys.ENTER)
         for _ in accounts:
             update()
+
+            spinner = driver.find_element_by_css_selector("div.dialog-aguarde")
+
+            status_dropdown = driver.find_element_by_xpath(
+                "//h4[contains(., 'Status:')]/following-sibling::div[@role='combobox']"
+            )
+            driver.execute_script(
+                "arguments[0].scrollIntoView();",
+                status_dropdown,
+            )
+            status_dropdown.click()
+
+            state_active_to_paused_changer.perform()
+            waiter.until(lambda d: "false" in spinner.get_attribute("aria-hidden"))
+            waiter.until(lambda d: "true" in spinner.get_attribute("aria-hidden"))
+
+            update()
+
             change_accounts(driver, accounts)
 
         print("everything done.")
