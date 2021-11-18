4b3d8f54e48987a1c58ed3de5709847fbaf06015
Mahesh <mahyess@users.noreply.github.com>
2021-11-18 21:47:19 +0545

    disable auto router reset removed

--- controllers/bots/belezanaweb.py
+++ controllers/bots/belezanaweb.py
@@ -309,7 +309,7 @@ def bot(root, details, driver=None):
                                 print(
                                     "******** if condition is matched. it has t/s in url."
                                 )
-                                root.is_reset_router_check.set(False)
+                                # root.is_reset_router_check.set(False)
 
                                 # getting this link is enough, so return here.. other code can stay here, doesn't matter
                                 return (
