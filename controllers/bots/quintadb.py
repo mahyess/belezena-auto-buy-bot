89cd710945d8b51f6400c65f81eed7913594bd72
Mahesh <mahyess@users.noreply.github.com>
2021-12-18 23:37:14 +0545

    quintadb update

--- controllers/bots/quintadb.py
+++ controllers/bots/quintadb.py
@@ -3,13 +3,9 @@ import datetime
 
 def quinta(driver, details):
     QUINTA_URL = (
-        "https://quintadb.com/widgets/bVWQ_cUrjckPZcIKfnB1jG/bfxtxcHmjiW4TlW67cISkK"
+        "https://quintadb.com/widgets/dcL8ktW4XbfRVdJSkNWRz8/a-j1RcGrrfl4obW6ZcNMyW"
     )
     driver.get(QUINTA_URL)
-
-    driver.find_element_by_xpath(
-        ".//label[contains(text(), 'Nome')]/following-sibling::input"
-    ).send_keys(f'{details["customer_first_name"]} {details["customer_last_name"]}')
 
     driver.find_element_by_xpath(
         ".//label[contains(text(), 'CPF')]/following-sibling::input"
@@ -24,16 +20,8 @@ def quinta(driver, details):
     ).send_keys(details["customer_email_password"])
 
     driver.find_element_by_xpath(
-        ".//label[contains(text(), 'Site')]/following-sibling::input"
-    ).send_keys(details["site"])
-
-    driver.find_element_by_xpath(
-        ".//label[contains(text(), 'Url')]/following-sibling::input"
+        ".//label[contains(text(), 'Link')]/following-sibling::input"
     ).send_keys(details["meurastre_url"])
-
-    driver.find_element_by_xpath(
-        ".//label[contains(text(), 'Data e Hora')]/following-sibling::input"
-    ).send_keys(datetime.now())
 
     driver.find_element_by_css_selector("input[type='submit']").click()
 
