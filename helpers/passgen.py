bfc8dd7639d0818359925dcccd416d87eaee1a53
Mahesh <mahyess@users.noreply.github.com>
2021-12-21 12:13:38 +0545

    passgen with certainty uppercase and digit

--- helpers/passgen.py
+++ helpers/passgen.py
@@ -2,7 +2,13 @@ import random
 import string
 
 
-def generate_password(length=8):
-    return "".join(
-        [random.choice(string.ascii_letters + string.digits) for _ in range(length)]
+def generate_password(length=None):
+    if length is None:
+        length = random.randint(6, 10)
+    return (
+        random.choice(string.ascii_uppercase)
+        + "".join(
+            [random.choice(string.ascii_letters + string.digits) for _ in range(length)]
+        )
+        + random.choice(string.digits)
     )
